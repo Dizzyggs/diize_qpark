68d032e9b69e2a02ef10fa9292597d16011835be
Diize <51971449+Diize@users.noreply.github.com>
2021-10-18 21:54:25 +0200

    update fxmanifest
    :)

--- diize_qpark/fxmanifest.lua
+++ diize_qpark/fxmanifest.lua
@@ -4,18 +4,8 @@ game 'gta5'
 
 author 'Diize'
 
-dependencies {
-  "zf_dialog"
-}
+dependencies {"zf_dialog"}
 
-client_script {
-  'client.lua',
-  'config.lua'
+client_scripts {'client.lua', 'config.lua'}
 
-}
-
-server_script {
-  '@mysql-async/lib/MySQL.lua',
-     'config.lua',
-     'server.lua'
- }
+server_scripts {'@mysql-async/lib/MySQL.lua', 'config.lua', 'server.lua'}
